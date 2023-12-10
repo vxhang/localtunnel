6686813262fc618101d86388d5960e6127265943
tao <rqrh1rjl1tl2n424g@163.com>
2023-12-10 22:44:04 +0800

    modify port

--- bin/lt.js
+++ bin/lt.js
@@ -2,17 +2,17 @@
 /* eslint-disable no-console */
 
 // const openurl = require('openurl');
-// const process = require('process');
+const process = require('process');
 
 
 const localtunnel = require('../localtunnel');
 // const { version } = require('../package');
 
-// console.log(process.argv);
+console.log(process.argv);
 
 const createTunnel = () =>
     localtunnel({
-      port: 7860,
+      port: process.argv[3],
       host: 'http://14.117.214.230:3010',
     }).then(tunnel => {
       console.log('your url is: %s', tunnel.url);
