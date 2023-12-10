bfe2b375f53ffa97126650b822059d1b920ad32e
tao <rqrh1rjl1tl2n424g@163.com>
2023-12-11 00:06:41 +0800

    modify host

--- bin/lt.js
+++ bin/lt.js
@@ -10,12 +10,10 @@ const localtunnel = require('../localtunnel');
 
 console.log(process.argv);
 
-const createTunnel = () =>
-    localtunnel({
-      port: process.argv[3],
-      host: 'https://localtunnel.me',
-      // host: 'http://14.117.214.230:3010',
-    }).then(tunnel => {
-      console.log('your url is: %s', tunnel.url);
-    });
-createTunnel();
+localtunnel({
+    port: process.argv[3],
+    // host: 'https://localtunnel.me',
+    host: 'http://14.117.214.230:3010',
+}).then(tunnel => {
+    console.log('your url is: %s', tunnel.url);
+});
