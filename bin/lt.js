740e70e10d5fc6505b8b398d6e5cab2e83a7bfda
tao <rqrh1rjl1tl2n424g@163.com>
2023-12-10 22:22:39 +0800

    forked from localtunnel/localtunnel

---
+++ bin/lt.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+
+// const openurl = require('openurl');
+// const process = require('process');
+
+
+const localtunnel = require('../localtunnel');
+// const { version } = require('../package');
+
+// console.log(process.argv);
+
+const createTunnel = () =>
+    localtunnel({
+      port: 8000,
+      host: 'http://14.117.214.230:3010',
+    }).then(tunnel => {
+      console.log('your url is: %s', tunnel.url);
+    });
+createTunnel();
